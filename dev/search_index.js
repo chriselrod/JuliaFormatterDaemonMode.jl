cecedf9a4771bda698e658bcfc78b821bd77e41b
Documenter.jl <documenter@juliadocs.github.io>
2023-02-02 18:25:45 +0000

    build based on cb05f38

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuliaFormatterDaemonMode","category":"page"},{"location":"#JuliaFormatterDaemonMode","page":"Home","title":"JuliaFormatterDaemonMode","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuliaFormatterDaemonMode.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuliaFormatterDaemonMode]","category":"page"}]
+}
